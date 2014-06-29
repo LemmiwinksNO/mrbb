a68e7af0b70d895fc6783fc5533b811ca73ed4dd
LemmiwinksNO <macreasy@gmail.com>
2014-06-29 18:06:12 -0500

    Added backbone.localstorage to play with. Changed main.js to hold requirejs configs, so I have one less file.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,48 @@
-require(["config"], function() {
+// Set require.js configuration for your application
+requirejs.config({
+  baseUrl: "assets/js",
 
-  require(["app"], function(App) {
-    App.start({environment: "development"});
-  });
+  paths: {
+    jquery: "../../vendor/jquery/dist/jquery",
+    underscore: "../../vendor/underscore/underscore",
+    backbone: "../../vendor/backbone/backbone",
+    marionette: "../../vendor/marionette/lib/backbone.marionette",
+    bootstrap: "../../vendor/bootstrap/dist/js/bootstrap",
+    handlebars: "../../vendor/handlebars/handlebars.runtime",
+    "backbone.syphon": "../../vendor/backbone.syphon/lib/backbone.syphon",
+    "backbone.wreqr": "../../vendor/backbone.wreqr/lib/backbone.wreqr",
+    "backbone.localstorage": "../../vendor/backbone.localstorage/backbone.localStorage.js",
 
+    // Require this to use compiled templates, it depends on HBS runtime.
+    templates: "../templates/templates",
+  },
+
+  shim: {
+    underscore: {
+      exports: "_"
+    },
+    backbone: {
+      deps: ["jquery", "underscore"],
+      exports: "Backbone"
+    },
+    "backbone.syphon": ["backbone"],
+    "backbone.wreqr": ["backbone"],
+    "backbone.localstorage": ["backbone"],
+    marionette: {
+      deps: ["backbone"],
+      exports: "Marionette"
+    },
+    bootstrap: ["jquery"],
+    handlebars: {
+      exports: "Handlebars"
+    },
+    templates: {
+      deps: ["handlebars"]
+    }
+  }
+});
+
+// Start the app
+require(["app"], function(App) {
+  App.start({environment: "development"});
 });
