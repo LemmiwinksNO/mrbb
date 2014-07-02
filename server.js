3486fc3a6d98453ed4703dfcdc5106b11abb1ae8
LemmiwinksNO <macreasy@gmail.com>
2014-07-01 23:23:12 -0500

    m

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
 
 var express   = require('express'),
+    logfmt = require("logfmt");
     // routes    = require('./routes'),
-    path      = require('path'),
-  mongoose  = require('mongoose');
+    // path      = require('path'),
+  // mongoose  = require('mongoose');
 
-mongoose.connect('mongodb://localhost/my_database');
+// mongoose.connect('mongodb://localhost/my_database');
 
-var app = module.exports = express();  // export so we can use it for tests
+var app = express();  // export so we can use it for tests
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 4000);
-  // app.set('env', process.env.NODE_ENV || process.argv[2] || 'development');  // Now you can set environment via command line.
-  app.set('env', 'production');
-  app.use(express.bodyParser());  // parses request body according to content type in request.
-  app.use(express.methodOverride());  // Lets you make HTTP methods other than GET and POST
-  app.use(app.router);
-  app.use(express.static(__dirname));  // lets you access everything rather than just /public or /app
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// app.configure(function(){
+//   app.set('port', process.env.PORT || 4000);
+//   // app.set('env', process.env.NODE_ENV || process.argv[2] || 'development');  // Now you can set environment via command line.
+//   app.set('env', 'production');
+//   app.use(express.bodyParser());  // parses request body according to content type in request.
+//   app.use(express.methodOverride());  // Lets you make HTTP methods other than GET and POST
+//   app.use(app.router);
+//   app.use(express.static(__dirname));  // lets you access everything rather than just /public or /app
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// app.configure('development', function(){
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
 
-app.configure('test', function(){
-  app.set('port', 4001);
-});
+// app.configure('test', function(){
+//   app.set('port', 4001);
+// });
 
-app.configure('production', function(){
-  app.use(express.static(path.join(__dirname + "/dist")));
-  app.use(express.errorHandler());
-  // app.set('port', 4002);
-});
+// app.configure('production', function(){
+//   app.use(express.static(path.join(__dirname + "/dist")));
+//   app.use(express.errorHandler());
+//   // app.set('port', 4002);
+// });
 
 // Routes - Controller code and URL handlers
 
 // require('./routes/index')(app);  // index
 // require('./routes/notdoing')(app, mongoose);  // not doing list
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
 // Start the app
 var port = Number(process.env.PORT || 5000);
