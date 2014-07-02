40f3e97336984ed1e9b959bafe8f749f4a7a1134
LemmiwinksNO <macreasy@gmail.com>
2014-07-02 00:33:43 -0500

    Everything should be working Heroku wise

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 // set up ======================================================================
 var express   = require('express');
 var app = express();
-// var mongoose  = require('mongoose');
+var mongoose  = require('mongoose');
 
 // configuration ===============================================================
-// mongoose.connect('mongodb://localhost/my_database');
+mongoose.connect('mongodb://localhost/my_database');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 4000);
   app.set('env', process.env.NODE_ENV || process.argv[2] || 'development');  // Now you can set environment via command line.
   app.use(express.static(__dirname));  // lets you access everything rather than just /public or /app
-  app.use(express.bodyParser());  // parses request body according to content type in request.
+  app.use(express.bodyParser());  // parses 1request body according to content type in request.
   app.use(express.methodOverride());  // Lets you make HTTP methods other than GET and POST
 });
 
-// app.configure('development', function(){
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-// });
-
-// app.configure('production', function(){
-//   app.use(express.errorHandler());
-//   app.set('port', 4002);
-// });
 
 // routes ======================================================================
 app.get('*', function(req, res) {
