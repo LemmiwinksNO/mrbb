0cd3e5a93667a38defe96e10e3c19c7886676ec2
LemmiwinksNO <macreasy@gmail.com>
2014-07-02 00:18:53 -0500

    m

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ var express   = require('express');
 
 var app = express();
 
-// app.configure(function(){
-//   app.set('port', process.env.PORT || 4000);
-//   // app.set('env', process.env.NODE_ENV || process.argv[2] || 'development');  // Now you can set environment via command line.
+app.configure(function(){
+  app.set('port', process.env.PORT || 4000);
+  app.set('env', process.env.NODE_ENV || process.argv[2] || 'development');  // Now you can set environment via command line.
 //   app.use(express.bodyParser());  // parses request body according to content type in request.
 //   app.use(express.methodOverride());  // Lets you make HTTP methods other than GET and POST
 //   app.use(app.router);
-//   app.use(express.static(__dirname));  // lets you access everything rather than just /public or /app
+  app.use(express.static(__dirname));  // lets you access everything rather than just /public or /app
 //   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-// });
+});
 
 // app.configure('development', function(){
 //   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -32,16 +32,12 @@ var app = express();
 //   // app.set('port', 4002);
 // });
 
-// Routes - Controller code and URL handlers
+// routes ======================================================================
 
 // require('./routes/index')(app);  // index
 // require('./routes/notdoing')(app, mongoose);  // not doing list
 
-// app.use(logfmt.requestLogger());
-// app.use(express.static(path.join(__dirname + "/dist")));
-
-// app.use(app.router);
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
 app.get('*', function(req, res) {
   res.sendfile('./index-prod.html');
@@ -49,10 +45,13 @@ app.get('*', function(req, res) {
 
 
 // Start the app
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function(){
-  console.log("listening on " + port);
-});
+// var port = Number(process.env.PORT || 5000);
 
-// app.listen(app.settings.port);
-// console.log("Express server listening on port %d in %s mode", app.settings.port, app.settings.env);
+// app.listen(port, function(){
+//   console.log("listening on " + port);
+// });
+
+// listen (start app with node server.js) ======================================
+app.listen(app.settings.port, function() {
+  console.log("Express server listening on port %d in %s mode", app.settings.port, app.settings.env);
+});
