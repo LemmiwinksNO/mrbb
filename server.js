ba355b13bc9bd90fbae60d763ba25136f8c49f45
LemmiwinksNO <macreasy@gmail.com>
2014-07-02 23:51:39 -0500

    m

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // set up ======================================================================
 var express   = require('express');
 var app = express();
-// var mongoose  = require('mongoose');
+var mongoose  = require('mongoose');
 
 
 // configuration ===============================================================
